af6a8602e8e43911e8bdbb5915f42ca076deec26
Atiqa Saif <atiqasaif674@gmail.com>
2026-06-09 20:40:41 -0700

    ABC

---
+++ script.js
@@ -0,0 +1,15 @@
+function sayHello(){
+    console.log("Hlo");
+}
+sayHello();
+
+function greet1(){
+    console.log("Greet");
+}
+greet1d();
+
+const name = "Atiqa"
+
+function newfn(){
+    console.log("hey im ati");  
+}
